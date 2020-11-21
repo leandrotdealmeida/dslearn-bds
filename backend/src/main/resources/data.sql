002d3b8ac4768e7bf6c881dc4167c1e2fe3bc849
devjapa <leandrotrovilho@gmail.com>
2020-11-21 18:07:24 -0300

    Add Course and Offer

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,3 +12,8 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp Quarkus', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpxhere.com%2Fpt%2Fphoto%2F1570023&psig=AOvVaw37IBILPCfeN1g_2a_NpA2r&ust=1606078041297000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLj8oIzBlO0CFQAAAAAdAAAAABAD', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fpt%2Fillustrations%2Fsearch%2Frabisco%2520linhas%2F&psig=AOvVaw2ysr1FYxtnKlBhZGAfEOXq&ust=1606078123688000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNiYpLXBlO0CFQAAAAAdAAAAABAD');
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z', 1);
