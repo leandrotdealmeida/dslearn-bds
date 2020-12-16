86344388b16257dbf799b24cde245ea3b65a6636
Leandro Almeida <leandrotrovilho@gmail.com>
2020-12-16 17:47:43 -0200

    feat(Notifications): create basic query for notifications

--- backend/src/main/java/com/devjapa/dslearn/repositories/NotificationRepository.java
+++ backend/src/main/java/com/devjapa/dslearn/repositories/NotificationRepository.java
@@ -1,9 +1,19 @@
 package com.devjapa.dslearn.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devjapa.dslearn.entities.Notification;
+import com.devjapa.dslearn.entities.User;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+	
+	@Query("SELECT obj FROM Notification obj WHERE "
+			+ "(obj.user = :user) AND "
+			+ "(obj.read = :read) "
+			+ "ORDER BY obj.moment DESC")
+	Page<Notification> find(User user, boolean read, Pageable pageable);
 
 }
