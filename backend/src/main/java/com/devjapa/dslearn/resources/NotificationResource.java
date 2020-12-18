f351ceabeaa3deb2dff8a46d224a4e9aa9116878
Leandro Almeida <leandrotrovilho@gmail.com>
2020-12-18 16:14:31 -0200

    feat(Notification): Custom content for current user, JPQL query

--- backend/src/main/java/com/devjapa/dslearn/resources/NotificationResource.java
+++ backend/src/main/java/com/devjapa/dslearn/resources/NotificationResource.java
@@ -3,7 +3,6 @@ package com.devjapa.dslearn.resources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,12 +22,13 @@ public class NotificationResource {
 	
 	@GetMapping
 	public ResponseEntity<Page<NotificationDTO>> findAll(
+			@RequestParam(value = "unreadOnly", defaultValue = "false") Boolean unreadOnly,
 			@RequestParam(value = "page", defaultValue = "0") Integer page,
 			@RequestParam(value = "size", defaultValue = "12") Integer size
 			) {
 		
 		PageRequest pageRequest = PageRequest.of(page, size);
-		Page<NotificationDTO> list = service.notificationsForCurrentUser(pageRequest);
+		Page<NotificationDTO> list = service.notificationsForCurrentUser(unreadOnly,pageRequest);
 		
 		return ResponseEntity.ok().body(list);
 	}
