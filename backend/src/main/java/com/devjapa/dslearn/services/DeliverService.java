780605bc8b5ef24acfe834c1e7433a59321a66ba
devjapa <leandrotrovilho@gmail.com>
2020-12-19 11:41:22 -0300

    Deliver Notification

--- backend/src/main/java/com/devjapa/dslearn/services/DeliverService.java
+++ backend/src/main/java/com/devjapa/dslearn/services/DeliverService.java
@@ -14,6 +14,9 @@ public class DeliverService {
 	@Autowired
 	private DeliverRepository deliverRepository;
 	
+	@Autowired
+	private NotificationService notificationService;
+	
 	@Transactional
 	public void saveDeliverRevision(Long id, DeliverRevisionDTO dto) {
 		Deliver deliver = deliverRepository.getOne(id);
@@ -21,6 +24,7 @@ public class DeliverService {
 		deliver.setFeedback(dto.getFeedback());
 		deliver.setCorrectCount(dto.getCorrectCount());
 		deliverRepository.save(deliver);
+		notificationService.saveDeliverNotification(deliver);
 	}
 
 }
--- backend/src/main/java/com/devjapa/dslearn/services/NotificationService.java
+++ backend/src/main/java/com/devjapa/dslearn/services/NotificationService.java
@@ -1,11 +1,15 @@
 package com.devjapa.dslearn.services;
 
+import java.time.Instant;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devjapa.dslearn.dto.NotificationDTO;
+import com.devjapa.dslearn.entities.Deliver;
 import com.devjapa.dslearn.entities.Notification;
 import com.devjapa.dslearn.entities.User;
 import com.devjapa.dslearn.repositories.NotificationRepository;
@@ -19,9 +23,27 @@ public class NotificationService {
 	@Autowired
 	private AuthService authService;
 	
+	@Transactional(readOnly = true)
 	public Page<NotificationDTO> notificationsForCurrentUser(boolean unreadOnly, Pageable pageable) {
 		User user = authService.authenticated();
 		Page<Notification> page = repository.find(user, unreadOnly, pageable);				
 		return page.map(x -> new NotificationDTO(x));		
+	}
+	
+	@Transactional
+	public void saveDeliverNotification(Deliver deliver) {
+		
+		Long sectionId = deliver.getLesson().getSection().getId();
+		Long resourceId = deliver.getLesson().getSection().getResource().getId();
+		Long offerId = deliver.getLesson().getSection().getResource().getOffer().getId();
+		
+		String route = "/offers/" + offerId + "/resources/" + resourceId + "/sections/" + sectionId;
+		String text = deliver.getFeedback();
+		Instant moment = Instant.now();
+		User user = deliver.getEnrollment().getStudent();
+		
+		Notification notification = new Notification(null, text, moment, false, route, user);
+		repository.save(notification);
+		
 	}
 }
